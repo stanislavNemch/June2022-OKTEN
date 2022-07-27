837c911a8015ed6a8201dfa92bc4102f83ec93bd
Stanislav <stas.buka@gmail.com>
2022-07-27 19:14:21 +0300

    Book Description Two is fixed, by discription array object!

--- js2022 lessons/Lessons2/Book Description Two/okten.js
+++ js2022 lessons/Lessons2/Book Description Two/okten.js
@@ -3,32 +3,34 @@ let bookObjectOne = {
     title: 'BookOne',
     pageCount: 756,
     genre: 'fantastic',
-    authors: {
-        name: 'NameOne',
-        age: 36
-    }
+    authors: [
+        {name: 'NameOne', age: 36},
+        {name: 'NameOne1', age: 40},
+        {name: 'NameOne2', age: 44}
+    ]
 }
 
 let bookObjectTwo = {
     title: 'BookTwo',
     pageCount: 654,
     genre: 'action',
-    authors: {
-        name: 'NameTwo',
-        age: 36
-    }
+    authors: [
+        {name: 'NameTwo', age: 36},
+        {name: 'NameTwo1', age: 40}
+    ]
 }
 
 let bookObjectThree = {
     title: 'BookThree',
     pageCount: 436,
     genre: 'thriller',
-    authors: {
-        name: 'NameThree',
-        age: 36
-    }
+    authors: [
+        {name: 'NameThree', age: 36},
+        {name: 'NameThree1', age: 40}
+    ]
 }
 
 console.log(bookObjectThree.authors.name);
 console.log(bookObjectOne.authors.age);
 console.log(bookObjectTwo.genre);
+console.log(bookObjectThree.authors[1].name)
