b746e666ff73849e127928f80548eb7af4b6b6df
Stanislav <stas.buka@gmail.com>
2022-09-14 16:05:55 +0300

    React

---
+++ june-2022-react/src/components/User.js
@@ -0,0 +1,12 @@
+import './User.css';
+export default function User() {
+    return (
+        <div>
+
+            <h2>user</h2>
+
+            <p>Sma text in this block</p>
+
+        </div>
+    );
+}
